b758c8213a54b0c8cdefbe8f23579f3137e80d55
Iulian PAUN <pauniulian3144@gmail.com>
2019-02-16 20:44:19 +0200

    add sql import script

---
+++ import_script.sql
@@ -0,0 +1,13 @@
+-- this script is used to import data from a sqlite database exported by MyWallet android application
+SELECT
+  exp_date as Date,
+  acc_name as Account
+  category_name as Category,
+  exp_note as Note,
+  exp_amount as Amount,
+  exp_is_debit as Debit
+FROM
+  tbl_trans, tbl_cat
+WHERE
+  exp_cat = category_id
+  exp_acc_id = acc_id
