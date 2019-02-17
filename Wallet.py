59b916e47e295561e5d3adb5b717918fa850e716
Iulian PAUN <pauniulian3144@gmail.com>
2019-02-17 11:46:15 +0200

    use amount instead of expense and income

--- Wallet.py
+++ Wallet.py
@@ -16,13 +16,13 @@ class Wallet:
                 date = datetime.strptime(row[0], '%Y%m%d')
                 category = row[1]
                 notes = row[2]
-                expense = income = 0
+                amount_value = float(row[3])
                 if int(row[4]) == 0:
-                    expense = float(row[3])
+                    amount = -amount_value
                 else:
-                    income = float(row[3])
+                    amount = amount_value
 
-                self.items.append(WalletItem(expense, income, date, notes, category))
+                self.items.append(WalletItem(amount, date, notes, category))
         self.items.sort(key=lambda x: x.date)
 
     def filter_items(self, summarize=False, filter_data=None):
@@ -36,21 +36,15 @@ class Wallet:
         if filter_data:
             filter_data = dict(item.split(':') for item in filter_data.split(','))
             if "category" in filter_data:
-                filtered_items = [item for item in filtered_items if re.match(filter_data["category"], item.category)]
+                filtered_items = [item for item in filtered_items if re.search(filter_data["category"], item.category)]
             if "notes" in filter_data:
-                filtered_items = [item for item in filtered_items if re.match(filter_data["notes"], item.notes)]
-            if "exp_min" in filter_data:
-                value = float(filter_data["exp_min"])
-                filtered_items = [item for item in filtered_items if item.expense >= value]
-            if "exp_max" in filter_data:
-                value = float(filter_data["exp_max"])
-                filtered_items = [item for item in filtered_items if item.expense <= value]
-            if "inc_min" in filter_data:
-                value = float(filter_data["inc_min"])
-                filtered_items = [item for item in filtered_items if item.income >= value]
-            if "inc_max" in filter_data:
-                value = float(filter_data["inc_max"])
-                filtered_items = [item for item in filtered_items if item.income <= value]
+                filtered_items = [item for item in filtered_items if re.search(filter_data["notes"], item.notes)]
+            if "amt_min" in filter_data:
+                value = float(filter_data["amt_min"])
+                filtered_items = [item for item in filtered_items if item.amount >= value]
+            if "amt_max" in filter_data:
+                value = float(filter_data["amt_max"])
+                filtered_items = [item for item in filtered_items if item.amount <= value]
             if "begin_date" in filter_data:
                 try:
                     begin_date = datetime.strptime(filter_data["begin_date"], '%d/%m/%Y')
@@ -68,24 +62,28 @@ class Wallet:
         expense_sum = 0
         income_sum = 0
         if not summarize:
-            print("{:10s} {:20s} {:30s} {:>10s} {:>10s}".format("Date", "Category", "Notes", "Expense", "Income"))
-            print("-" * 85)
+            print("{:10s} {:20s} {:30s} {:>10s}".format("Date", "Category", "Notes", "Amount"))
+            print("-" * 73)
         for item in filtered_items:
-            expense_sum += item.expense
-            income_sum += item.income
+            if item.amount < 0:
+                expense_sum += abs(item.amount)
+            else:
+                income_sum += item.amount
             if not summarize:
                 print(item)
 
         expense_sum = round(expense_sum, 2)
         income_sum = round(income_sum, 2)
         if not summarize:
-            print("-" * 85)
-        print("{:>62s}: \033[0;31m{:10.2f}\033[0m \033[0;32m{:10.2f}\033[0m".format(
-            "TOTAL", expense_sum, income_sum))
+            print("-" * 73)
+
+        print("{:>61s}: \033[0;31m{:10.2f}\033[0m".format("EXPENSE", expense_sum))
+        print("{:>61s}: \033[0;32m{:10.2f}\033[0m".format("INCOME", income_sum))
         savings = income_sum - expense_sum
-        print("{:>62s}: ".format("SAVINGS"), end='')
-        if savings >= 0:
-            format_str = "\033[0;32m{:10.2f}\033[0m"
+
+        format_str = "{:>61s}: "
+        if savings < 0:
+            format_str += "\033[0;31m{:10.2f}\033[0m"
         else:
-            format_str = "\033[0;31m{:10.2f}\033[0m"
-        print(format_str.format(savings))
+            format_str += "\033[0;32m{:10.2f}\033[0m"
+        print(format_str.format("SAVINGS", savings))
--- WalletItem.py
+++ WalletItem.py
@@ -7,23 +7,25 @@ class WalletItem:
       0 - expense
       1 - income
     """
-    def __init__(self, expense, income, date, notes="", category=""):
+    def __init__(self, amount, date, notes="", category=""):
         """
-        :param expense: expense value
-        :param income: income value
+        :param amount: income value
         :param notes: notes string
         :param category: category name
         """
-        self.expense: float = expense
-        self.income: float = income
+        self.amount: float = amount
         self.date: datetime = date
         self.notes: str = notes
         self.category: str = category
 
     def __str__(self):
-        return '{:10s} {:20s} {:30s} \033[0;31m{:10.2f}\033[0m \033[0;32m{:10.2f}\033[0m'.format(
+        format_str = '{:10s} {:20s} {:30s} '
+        if self.amount < 0:
+            format_str += '\033[0;31m{:10.2f}\033[0m'
+        else:
+            format_str += '\033[0;32m{:10.2f}\033[0m'
+        return format_str.format(
             self.date.strftime('%d/%m/%Y'),
             self.category,
             self.notes[:30],
-            self.expense,
-            self.income)
+            self.amount)
